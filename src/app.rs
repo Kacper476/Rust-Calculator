f94c9a045ac960e222f51dc103e075d63e3c80ec
Kacper476 <kacpervfr123@gmail.com>
2024-07-30 20:48:32 +0200

    fun

--- src/app.rs
+++ src/app.rs
@@ -1,19 +1,58 @@
+// src/app.rs
 use eframe::egui;
 
 pub struct MyApp {
-    counter: i32,
+    pub counter: f32, // Używamy typu f32 dla wartości zmiennoprzecinkowych
 }
 
 impl Default for MyApp {
     fn default() -> Self {
-        Self { counter: 2137 }
+        Self { counter: 2137.0 }
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            crate::ui_counter::ui_counter(ui, &mut self.counter);
+            ui.with_layout(egui::Layout::left_to_right(egui::Align::Center), |ui| {
+                ui.add_space(250.0);
+                ui_counter(ui, &mut self.counter);
+                //ui_counter(ui, &mut self.counter);
+            });
         });
+
+
+
+
+
+        
     }
+}
+
+fn ui_counter(ui: &mut egui::Ui, counter: &mut f32) {
+    ui.horizontal(|ui| {
+        ui.label(format!("Counter: {:.1}", counter));
+
+        ui.add(egui::Slider::new(counter, 0.0..=10000.0)
+            .text("Counter")
+            .clamp_to_range(true)
+            .step_by(1.1)
+            .show_value(false)); 
+
+        
+    });
+
+    ui.vertical(|ui| {
+        ui.add_space(120.0);
+        ui.label(format!("Counter: {:.1}", counter));
+
+        ui.add(egui::Slider::new(counter, 0.0..=10000.0)
+            .text("Counter")
+            .clamp_to_range(true)
+            .step_by(1.1)
+            .show_value(false)); 
+
+            ui.label(format!("Counter: {:.1}", counter));
+    });
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-
-mod app;
-mod ui_counter;
+// src/main.rs
+use eframe::egui;
+mod app; // Importujemy moduł app
 
 fn main() -> Result<(), eframe::Error> {
-    let options = eframe::NativeOptions::default();
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(600.0, 400.0)), 
+        ..Default::default()
+    };
     eframe::run_native(
         "Display Counter App",
         options,
-        Box::new(|_cc| Box::new(app::MyApp::default())),
+        Box::new(|_cc| Box::<app::MyApp>::default()), 
+        
     )
 }
